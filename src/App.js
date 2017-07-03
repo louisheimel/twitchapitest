1da41b76b5ddfc74b8a91458b6b8f81619a25f2b
louisheimel1@gmail.com <louisheimel1@gmail.com>
2017-07-02 22:07:43 -0500

    add test component to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Test from './components/Test.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Test />
       </div>
     );
   }
